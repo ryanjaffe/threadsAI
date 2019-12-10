10060872683180edb7edeeeb49e06d90a5e2fd1b
Ryan Jaffe <ryancjaffe@gmail.com>
2019-12-10 01:41:41 -0600

    added renderHome and handleGetStarted functions

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 function renderHome(){
+    $('.js-photo-screen').hide();
     $('.js-preview-screen').hide();
     $('.js-palette-select-screen').hide();
     $('.js-palette-display-screen').hide();
+    
+    handleGetStarted();
 }
+
+function handleGetStarted(){
+    $('.js-get-started-button').on('click',function(event){
+        $('.js-instructions-screen').hide();
+        $('.js-photo-screen').show();
+    })
+}
+renderHome();
