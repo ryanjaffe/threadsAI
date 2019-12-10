ea8dd888b6c7d4866644193b0ea00115b75c4896
Ryan Jaffe <ryancjaffe@gmail.com>
2019-12-10 12:17:28 -0600

    added handleInstructionsButton, handlePhotoApprovedButton, and handlePhotoButton functions

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 function renderHome(){
     $('.js-photo-screen').hide();
-    $('.js-preview-screen').hide();
+    $('.js-extraction-preview-screen').hide();
     $('.js-palette-select-screen').hide();
     $('.js-palette-display-screen').hide();
 
     handleGetStarted();
+}
+
+function handleGetStarted(){
+    $('.js-get-started-button').on('click',function(event){
+        $('.js-instructions-screen').hide();
+        $('.js-preview-heading').hide();
+        $('.js-photo-screen').show();
+    });
 }
 
 Webcam.attach( '.js-viewfinder' );
@@ -13,14 +21,33 @@ function take_snapshot() {
         document.getElementById('my_result').innerHTML = 
         '<img src="'+data_uri+'"/>';
     } );
+    $('.js-preview-heading').show();
+    handlePhotoApprovedButton();
 }
 
-function handleGetStarted(){
-    $('.js-get-started-button').on('click',function(event){
+function handleInstructionsButton(){
+    $('.js-instructions-button').on('click',function(event){
+        $('.js-instructions-screen').show();
+        $('.js-photo-screen').hide();
+        $('.js-extraction-preview-screen').hide();   
+    });
+}
+
+function handlePhotoApprovedButton(){
+    $('.js-photo-approved').on('click',function(event){
+        $('.js-photo-screen').hide();
+        $('.js-extraction-preview-screen').show();
+    });
+    handlePhotoButton();
+}
+
+function handlePhotoButton(){
+    $('.js-photo-button').on('click',function(event){
         $('.js-instructions-screen').hide();
         $('.js-photo-screen').show();
-    })
+        $('.js-extraction-preview-screen').hide();   
+    });
 }
 
-
+handleInstructionsButton();
 renderHome();
