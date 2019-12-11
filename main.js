5f102aadc38ead03a8c7add678e6afc92db0e2dd
Ryan Jaffe <ryancjaffe@gmail.com>
2019-12-11 15:05:37 -0600

    added imagga api for image color extraction

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+'use strict'
+
+const extractionUrl = 'https://api.imagga.com/v2/colors';
+const extractionAuthorization = 'Basic YWNjXzNkNzBmYTkxNjFlMzAwOTo5OWRlNTExN2E0MmJkMzY5NDMzM2UzZjYzMGZhYzQ0Mw==';
+
 function renderHome(){
     $('.js-photo-screen').hide();
     $('.js-extraction-preview-screen').hide();
@@ -20,10 +25,29 @@ function take_snapshot() {
     Webcam.snap( function(data_uri) {
         document.getElementById('my_result').innerHTML = 
         '<img src="'+data_uri+'"/>';
+        extractColors(data_uri);
+        console.log(data_uri);
     } );
     $('.js-preview-heading').show();
     handlePhotoApprovedButton();
 }
+
+
+function extractColors(data_uri) {
+    const url = `${extractionUrl}?image_url=${data_uri}&extract_overall_colors=0
+    extract_object_colors=1&overall_count=3`;
+  
+   const extractionHeaders = {
+        headers: new Headers({
+        'authorization' : extractionAuthorization
+        })
+    };
+  
+    fetch(extractionUrl, extractionHeaders)
+      .then(response => response.json())
+      .then(responseJson => console.log(responseJson));
+}  
+
 
 function handleInstructionsButton(){
     $('.js-instructions-button').on('click',function(event){
